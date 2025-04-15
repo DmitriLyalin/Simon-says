5267db04d923bd7e48d1a3d8401a4ef853648931
DmitriLyalin <donlyalin@gmail.com>
2025-04-15 14:52:25 +0300

    feat: implement keyboard highlighting and keypress validation with alphanumeric support

--- src/scripts/event_listeners.js
+++ src/scripts/event_listeners.js
@@ -1,11 +1,19 @@
 import { levelContainer } from "./generate_elements.js";
 import { createKeyboard } from "./keyboard.js";
 import { button } from "./generate_elements.js";
+import { startHighlighting } from "./functions.js";
+import { highlightedDivs } from "./functions.js";
+import { checkKeys } from "./functions.js";
+import { counterManager } from "./functions.js";
+import { counterState } from "./functions.js";
+
+
 
 
 levelContainer.addEventListener("click", (e) => {
   if (e.target.innerText === "easy") {
     createKeyboard('easy');
+
   }
   if (e.target.innerText === "medium") {
     createKeyboard('medium');
@@ -15,6 +23,42 @@ levelContainer.addEventListener("click", (e) => {
   }
 });
 
-button.addEventListener("click", (e) => {
+// button.addEventListener("click", (e) => {
+//   startHighlighting(2)
+//   console.log('All selected divs:', highlightedDivs);
+//   setTimeout(() => {
+//     document.addEventListener('keydown', (event) => {
+//       const pressedKey = event.key;
 
-})
+//       const isAlphanumeric = /^[a-zA-Z0-9\u0400-\u04FF\u0500-\u052F]$/;
+//       if (isAlphanumeric.test(event.key)) {
+//         checkKeys(pressedKey);
+//       }
+//     });
+//     console.log('Keydown event listener added after delay');
+//   }, 3000); // 3000ms = 3 seconds
+// })
+
+button.addEventListener("click", (e) => { // При нажатии на кнопку вызывается функция startHighlighting(2) для подсветки двух случайных элементов на странице.
+  startHighlighting(2);
+  console.log('All selected divs:', highlightedDivs);
+
+  setTimeout(() => { //После задержки в 3 секунды (3000ms) добавляет обработчик событий на keydown.
+    const keydownHandler = (event) => { // Отслеживает нажатие клавиш, используя event.key.
+      const pressedKey = event.key;
+
+      const isAlphanumeric = /^[a-zA-Z0-9\u0400-\u04FF\u0500-\u052F]$/; // Проверяет, является ли нажатая клавиша буквенно-цифровой (включая кириллические символы) через регулярное выражение isAlphanumeric.
+      if (isAlphanumeric.test(event.key)) {
+        // Если checkKeys возвращает false (например, достигнут конец подсветки или найдено совпадение), удаляет обработчик событий keydown, чтобы остановить дальнейшее выполнение.
+        const result = checkKeys(pressedKey);
+        if (!result) {
+          console.log("Condition met, removing keydown listener.");
+          document.removeEventListener('keydown', keydownHandler);
+        }
+      }
+    };
+
+    document.addEventListener('keydown', keydownHandler);
+    console.log('Keydown event listener added after delay');
+  }, 3000); // 3000ms = 3 seconds
+});
--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -4,16 +4,66 @@ import { keyboardWrapper } from "./generate_elements.js"
 import { button } from "./generate_elements.js";
 import { letters } from "./keyboard.js"
 import { digits } from "./keyboard.js"
+
+ // Эта функция принимает массив (arr) и возвращает случайный элемент из него.
 const getRandomElement = (arr) => {
   const randomIndex = Math.floor(Math.random() * arr.length);
   return arr[randomIndex];
 };
-
-const highlightRandomElement = () => {
-  // let randomNumber = getRandomElement(digits);
-  const divs = Array.from(document.querySelectorAll('.keyboard__key'));
-  console.log(divs)
+ // Подсвечивает случайный элемент на странице.
+export const highlightRandomElement = () => {
+  const divs = Array.from(document.querySelectorAll('.keyboard__key')); // Преобразует их в массив (Array.from) для работы с функцией getRandomElement.
   let randomDiv = getRandomElement(divs);
-  randomDiv.style.color = "green"
+  randomDiv.style.color = "red";
+  return randomDiv;
 }
+export const highlightedDivs = []; // Массив, предназначенный для хранения всех подсвеченных элементов.
+export const startHighlighting = (maxCount) => { // Функция запускает процесс подсвечивания элементов, ограниченный количеством maxCount.
+
+  let count = 0;
+  const intervalId = setInterval(() => { //Используется setInterval для выполнения действий с интервалом в 1000 мс (1 секунда).
+    let selectedDiv = highlightRandomElement()
+    highlightedDivs.push(selectedDiv);
+    count++
+    if (count === maxCount) {
+      clearInterval(intervalId); //Если счетчик достигает значения maxCount, интервал останавливается с помощью clearInterval
+
+    }
+  }, 1000);
+};
+
+export const counterManager = () => { //Функция управляет состоянием счетчика и возвращает объект с методами:
+  let counter = 0; // Private state
+  return {
+    get: () => counter, // Возвращает текущее значение счетчика.
+    increment: () => counter++, // Увеличивает счетчик на единицу.
+  };
+};
+export const counterState = counterManager(); // Создает замыкание. Объяект в через методы котороого мы получаем доступ к счетчику
+
+export const checkKeys = (pressedKey) => { //Функция проверяет, соответствует ли нажатая клавиша тексту в подсвеченных элементах:
+  const input = document.querySelector('.input');
+  let isCorrect = false; // Состояние отслеживает правильно ли все нажато
+
+  for (let i = counterState.get(); i < highlightedDivs.length; i++) {
+    if (pressedKey.toLowerCase() === highlightedDivs[i].innerText.toLowerCase()) {
+      console.log(`Correct! You pressed: ${pressedKey}`)
+      counterState.increment();
+
+      input.value += pressedKey;
+
+      return isCorrect = true;
+
+    }
+    else {
+      input.value += pressedKey; 
+      console.log(`Wrong key. You pressed: ${pressedKey}`);
+      return isCorrect = false;
+
+    }
+  }
+
+};
+
+
 
